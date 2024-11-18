b012758186f3280ff7d12d25eb49790a1a16bddc
Tudor Filimon <tfilimon123@gmail.com>
2024-11-17 22:17:20 -0500

    COmmit

--- S5: Logic and Control Flow/01-operators/script.js
+++ S5: Logic and Control Flow/01-operators/script.js
@@ -1,3 +1,4 @@
+// If else statements
 if (true) {
   console.log('This is true');
 }
@@ -19,3 +20,46 @@ if (x !== y) {
   const z = 20;
   console.log(`${y} is 20`);
 }
+
+// if, else if, else
+
+const d = new Date(10, 30, 2022, 8, 0, 0);
+const hour = d.getHours();
+
+console.log(hour);
+
+if (hour < 12) {
+  console.log('Good Morning');
+} else if (hour < 18) {
+  console.log('Good Afternoon');
+} else {
+  console.log('Good Night');
+}
+
+// Nested If Statements
+if (hour < 12) {
+  console.log('Good Morning');
+  if (hour === 6) {
+    console.log('Wake Up');
+  }
+} else if (hour < 18) {
+  console.log('Good Afternoon');
+} else {
+  console.log('Good Night');
+
+  if (hour >= 20) {
+    console.log('zzzzzzzz');
+  }
+}
+
+if (hour >= 7 && hour < 15) {
+  //&& operator
+  console.log('It is work time!');
+}
+
+if (hour === 6 || hour === 20) {
+  // || operator
+  console.log('Brush your teeth!');
+}
+
+// However, if you begin to nest a lot, use a switch
